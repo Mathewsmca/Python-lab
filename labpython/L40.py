d12bdfe5cb1b21f526c12ec746b066150ed6e0f3
Mathewsmca <98259029+Mathewsmca@users.noreply.github.com>
2022-03-06 13:18:03 +0530

    Add files via upload

---
+++ labpython/L40.py
@@ -0,0 +1,13 @@
+import csv
+
+with open("csv", newline='') as csvfile:
+
+    d = csv.DictReader(csvfile)
+
+print("Period       Subject")
+
+print("--------------------")
+
+    for i in d:
+
+        print(i['Period'], i['Subject'])
---
+++ labpython/L5.py
@@ -0,0 +1,5 @@
+list1 = str(['ram', 'appu', 'ramu'])
+print("list1 : ", list1)
+count = list1.count("a")
+print("Occurance of 'a' = ", count)
+
